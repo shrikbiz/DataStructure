d9d445aba928475411658867423d10244709a939
Shrikant Patel <shrikantpatel@Shrikants-MacBook-Air.local>
2020-06-29 01:38:34 -0600

    AVL fully done   B-)

--- Tree/AVLTree.js
+++ Tree/AVLTree.js
@@ -5,16 +5,6 @@ class AVLTree {
   constructor() {
     this.root = null;
   }
-  //   insertValue(parent, value, height) {
-  //     if (!parent) {
-  //       this.height = this.height < height ? height : this.height;
-  //       return new Node(value);
-  //     }
-  //     parent.value > value
-  //       ? (parent.left = this.insertValue(parent.left, value, height + 1))
-  //       : (parent.right = this.insertValue(parent.right, value, height + 1));
-  //     return parent;
-  //   }
 
   insert(value) {
     this.root = this.insertValue(this.root, value);
@@ -24,6 +14,7 @@ class AVLTree {
     if (!parent) {
       return new Node(value);
     }
+
     if (parent.value > value) {
       parent.left = this.insertValue(parent.left, value);
     } else {
@@ -31,63 +22,74 @@ class AVLTree {
     }
 
     parent.height =
-      1 + Math.max(this.height(parent.left), this.height(parent.right));
+      1 + Math.max(this.isChild(parent.left), this.isChild(parent.right));
 
-    parent = this.balance(parent);
+    parent = this.treeBalancer(parent);
 
-    parent.height =
-      1 + Math.max(this.height(parent.left), this.height(parent.right));
     return parent;
   }
 
-  balance(parent) {
+  treeBalancer(parent) {
     let newRoot;
+    let temp;
+
     if (this.isLeftHeavy(parent)) {
+      if (this.balanceFactor(parent.left) < 0) {
+        temp = parent.left;
+        parent.left = temp.right;
+        temp.right = null;
+        parent.left.left = temp;
+      }
       newRoot = parent.left;
-
       parent.left = newRoot.right;
-
       newRoot.right = parent;
-
+      newRoot = this.heightBalancer(newRoot);
       return newRoot;
-
-      //   let leftBalance = this.balanceFactor(parent.left);
-      //   if (leftBalance < 0) console.log("Left Rotation on", parent.left.value);
-      //   console.log("Right Rotation on ", parent.value);
     } else if (this.isRightHeavy(parent)) {
+      if (this.balanceFactor(parent.right) > 0) {
+        temp = parent.right;
+        parent.right = temp.left;
+        temp.left = null;
+        parent.right.right = temp;
+      }
       newRoot = parent.right;
-
       parent.right = newRoot.left;
-
       newRoot.left = parent;
+      newRoot = this.heightBalancer(newRoot);
       return newRoot;
-      //   let rightBalance = this.balanceFactor(parent.right);
-      //   if (rightBalance > 0)
-      //     console.log("Right Rotation on", parent.right.value);
-      //   console.log("Left Rotation on", parent.value);
     }
+    return parent;
+  }
+
+  heightBalancer(parent) {
+    if (!parent) return -1;
+
+    this.heightBalancer(parent.left);
+    this.heightBalancer(parent.right);
+
+    parent.height =
+      1 + Math.max(this.isChild(parent.left), this.isChild(parent.right));
     return parent;
   }
 
   isLeftHeavy(node) {
     return this.balanceFactor(node) > 1;
   }
+
   isRightHeavy(node) {
     return this.balanceFactor(node) < -1;
   }
 
   balanceFactor(node) {
-    return (
-      (node.left ? node.left.height : 0) - (node.right ? node.right.height : 0)
-    );
+    return this.isChild(node.left) - this.isChild(node.right);
   }
 
-  height(node) {
+  isChild(node) {
     return !node ? -1 : node.height;
   }
 
   display() {
-    console.log("tree =>", this.root);
+    console.log("AVLTree -> display -> this.root", this.root);
     return this.root;
   }
 }
@@ -95,7 +97,7 @@ class AVLTree {
 class Node {
   constructor(value) {
     this.value = value;
-    this.height = 1;
+    this.height = 0;
     this.left = null;
     this.right = null;
   }
@@ -104,18 +106,22 @@ class Node {
 let avl = new AVLTree();
 
 avl.insert(10);
-avl.insert(20);
 avl.insert(30);
-// avl.insert(11);
-// avl.insert(9);
-// avl.insert(15);
-// avl.insert(13);
-// avl.insert(5);
+avl.insert(20);
+avl.insert(40);
+avl.insert(35);
+avl.insert(60);
+avl.insert(70);
+avl.insert(11);
+avl.insert(9);
+avl.insert(15);
+avl.insert(13);
+avl.insert(5);
+avl.insert(6);
+avl.insert(4);
+avl.insert(8);
+avl.insert(20);
+avl.insert(1);
+avl.insert(22);
+avl.insert(21);
 avl.display();
-// avl.insert(6);
-// avl.insert(4);
-// avl.insert(8);
-// avl.insert(20);
-// avl.insert(1);
-// avl.insert(22);
-// avl.insert(21);
